539ae471e891f99507e6872f35d8034af11b05cc
FireFoxi19 <78423934+FireFoxi19@users.noreply.github.com>
2022-05-22 20:30:58 +0700

    add 4 stage

---
+++ RGRAvalonia/RGRAvalonia/Models/PlaceOfTheRace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGRAvalonia
+{
+    public partial class PlaceOfTheRace
+    {
+        public DateTime Date { get; set; }
+        public int NumParticipans { get; set; }
+        public string PlaceName { get; set; }
+        public int Num_Winner { get; set; }
+
+        public virtual Resault Resault { get; set; }
+    }
+}
