6729a4df2c39a2a63d2d8062ddfbdcebdfbff859
FireFoxi19 <78423934+FireFoxi19@users.noreply.github.com>
2022-05-22 20:32:19 +0700

    add 4 stage

---
+++ RGRAvalonia/RGRAvalonia/Models/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sysytem.Collections.Generic;
+
+namespace RGRAvalonia
+{
+    public partial class Trainer
+    {
+        public Tainer()
+        {
+            StatisticDog = new HashSet<StatsDog>();
+        }
+
+        public string Name { get; set; }
+        public int Num_runners { get; set; }
+    }
+
+    public virtual ICollection<StatsDod>StatisticDog { get; set; }
+}
---
+++ RGRAvalonia/RGRAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RGRAvalonia_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
