1451ddab4917f5f93ddaa2e41cf28332e353d0bf
FireFoxi19 <78423934+FireFoxi19@users.noreply.github.com>
2022-05-27 22:08:08 +0700

    add interface but need fix bug

---
+++ RGRAvalonia/RGRAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using RGRAvalonia.ViewModels;
+
+namespace RGRAvalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
