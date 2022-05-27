af613ef1ba22eabf09d4c67bea1f24622e8c4b45
FireFoxi19 <78423934+FireFoxi19@users.noreply.github.com>
2022-05-27 22:06:19 +0700

    add interface но надо фиксить баг

---
+++ RGRAvalonia/RGRAvalonia/RGRAvalonia/Models/Результат.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGRAvalonia
+{
+    public partial class Результат
+    {
+        public byte[]? Date { get; set; }
+        public byte[]? NickName { get; set; }
+        public long? StartPosition { get; set; }
+        public byte[]? TimeRace { get; set; }
+        public long? Finish { get; set; }
+    }
+}
